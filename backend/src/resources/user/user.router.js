e28f8eab0af0b7db9dbb2be1344f66f7ce599676
Abhisar Shukla <abhisarshukla645@gmail.com>
2022-02-04 15:46:21 +0530

    Add RBAC to user routes

--- backend/src/resources/user/user.router.js
+++ backend/src/resources/user/user.router.js
@@ -1,9 +1,12 @@
 import { Router } from 'express'
-import { me, updateMe } from './user.controller'
+import { me, updateMe, updateUser, getUsers } from './user.controller'
+import { grantAcess } from '../../roles/roles.middleware'
 
 const router = new Router()
 
-router.get('/', me)
-router.put('/', updateMe)
+router.get('/', grantAcess('readOwn', 'profile'), me)
+router.put('/', grantAcess('updateOwn', 'profile'), updateMe)
+router.put('/:id', grantAcess('updateAny', 'user'), updateUser)
+router.get('/all', grantAcess('readAny', 'user'), getUsers)
 
 export default router
