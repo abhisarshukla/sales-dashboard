1b25ddd4ae08f825bb4c02c29df94a68e8e70387
Abhisar Shukla <abhisarshukla645@gmail.com>
2022-02-05 17:24:56 +0530

    Correct Spelling in user router

--- backend/src/resources/user/user.router.js
+++ backend/src/resources/user/user.router.js
@@ -1,12 +1,12 @@
 import { Router } from 'express'
 import { me, updateMe, updateUser, getUsers } from './user.controller'
-import { grantAcess } from '../../roles/roles.middleware'
+import { grantAccess } from '../../roles/roles.middleware'
 
 const router = new Router()
 
-router.get('/', grantAcess('readOwn', 'profile'), me)
-router.put('/', grantAcess('updateOwn', 'profile'), updateMe)
-router.put('/:id', grantAcess('updateAny', 'user'), updateUser)
-router.get('/all', grantAcess('readAny', 'user'), getUsers)
+router.get('/', grantAccess('readOwn', 'profile'), me)
+router.put('/', grantAccess('updateOwn', 'profile'), updateMe)
+router.put('/:id', grantAccess('updateAny', 'user'), updateUser)
+router.get('/all', grantAccess('readAny', 'user'), getUsers)
 
 export default router
