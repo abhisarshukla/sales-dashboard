8b8b64373b2ef40e1ddf6fa68c178b6b7bdcd3ac
Abhisar Shukla <abhisarshukla645@gmail.com>
2022-02-08 23:18:00 +0530

    Formatting

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -19,6 +19,11 @@ const routes: Routes = [
     loadChildren: () =>
       import('./sales/sales.module').then((m) => m.SalesModule),
   },
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./products/products.module').then((m) => m.ProductsModule),
+  },
 ];
 
 @NgModule({
