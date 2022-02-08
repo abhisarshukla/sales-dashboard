73373cdd57f0d4670ca0d1f6908f184d2827676a
Abhisar Shukla <abhisarshukla645@gmail.com>
2022-02-08 23:17:06 +0530

    Fix router order

--- backend/src/resources/product/product.router.js
+++ backend/src/resources/product/product.router.js
@@ -3,8 +3,8 @@ import { getOne, getAll, update, remove, create } from './product.controller'
 import { grantAccess } from '../../roles/roles.middleware'
 
 const router = new Router()
-router.get('/:id', grantAccess('readAny', 'product'), getOne)
 router.get('/all', grantAccess('readAny', 'product'), getAll)
+router.get('/:id', grantAccess('readAny', 'product'), getOne)
 router.put('/:id', grantAccess('updateAny', 'product'), update)
 router.delete('/:id', grantAccess('deleteAny', 'product'), remove)
 router.post('/', grantAccess('createAny', 'product'), create)
