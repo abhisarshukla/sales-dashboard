003e91e90187669d8714ae97c90765f54e2b7bf0
Abhisar Shukla <abhisarshukla645@gmail.com>
2022-02-09 16:52:44 +0530

    Add get and role toggle routes for user

--- backend/src/resources/user/user.controller.js
+++ backend/src/resources/user/user.controller.js
@@ -76,3 +76,38 @@ export const remove = async (req, res) => {
     res.status(400).end()
   }
 }
+
+export const getOne = async (req, res) => {
+  try {
+    const user = await User.findOne({ user_id: req.params.id }).lean().exec()
+    res.status(200).json({ data: user })
+  } catch (e) {
+    console.error(e)
+    res.status(400).end()
+  }
+}
+
+export const toggleRole = async (req, res) => {
+  try {
+    const user = await User.findOne({ user_id: req.params.id }).lean().exec()
+    let nextRole = ''
+    switch (user.role) {
+      case 'admin':
+        nextRole = 'sales'
+        break
+      case 'sales':
+        nextRole = 'curator'
+        break
+      case 'curator':
+        nextRole = 'admin'
+        break
+    }
+    await Order.findOneAndUpdate({ user_id: req.params.id }, { role: nextRole })
+      .lean()
+      .exec()
+    res.status(200).end()
+  } catch (e) {
+    console.error(e)
+    res.status(400).end()
+  }
+}
--- backend/src/resources/user/user.router.js
+++ backend/src/resources/user/user.router.js
@@ -1,5 +1,13 @@
 import { Router } from 'express'
-import { me, updateMe, updateUser, getUsers, remove } from './user.controller'
+import {
+  me,
+  updateMe,
+  updateUser,
+  getUsers,
+  remove,
+  getOne,
+  toggleRole,
+} from './user.controller'
 import { grantAccess } from '../../roles/roles.middleware'
 
 const router = new Router()
@@ -7,7 +15,9 @@ const router = new Router()
 router.get('/', grantAccess('readOwn', 'profile'), me)
 router.put('/', grantAccess('updateOwn', 'profile'), updateMe)
 router.get('/all', grantAccess('readAny', 'user'), getUsers)
+router.get('/:id', grantAccess('readAny', 'user'), getOne)
 router.put('/:id', grantAccess('updateAny', 'user'), updateUser)
 router.delete('/:id', grantAccess('deleteAny', 'user'), remove)
+router.put('/toggleRole/:id', grantAccess('updateAny', 'user'), toggleRole)
 
 export default router
